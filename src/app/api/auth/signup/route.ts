38a0ab2f1dc1a3a7ea8240fa556b7f4322bd6270
José <jg345762@gmail.com>
2023-09-20 16:47:01 -0300

    route updated

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -7,14 +7,12 @@ export default async function POST(req: NextRequest) {
   try {
     await connectDB();
     const { username, email, password } = await req.json();
-
     if (!password || password.length < 6) {
       return NextResponse.json(
         { message: "This field must be at least 6 characters" },
         { status: 400 }
       );
     }
-
     const userFound = await User.findOne({ email });
     if (userFound) {
       return NextResponse.json(
@@ -22,14 +20,14 @@ export default async function POST(req: NextRequest) {
         { status: 400 }
       );
     }
-
     const hashedPassword = await bcrypt.hash(password, 12);
     const newUser = new User({ username, password: hashedPassword, email });
     const userSaved = await newUser.save();
     return NextResponse.json(userSaved);
   } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json({ message: error.message });
-    }
+    return NextResponse.json({ message: "Error" });
+    // if (error instanceof Error) {
+    //   return NextResponse.json({ message: error.message });
+    // }
   }
 }
