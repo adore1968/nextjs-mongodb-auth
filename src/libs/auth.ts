fc3b6abf2acdbaa4ec93057af5f382e8cef05be8
José <jg345762@gmail.com>
2023-09-20 16:25:21 -0300

    next-auth updated

---
+++ src/libs/auth.ts
@@ -0,0 +1,63 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import connectDB from "@/database/connection";
+import User from "@/database/models/User";
+import { NextAuthOptions } from "next-auth";
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "******",
+        },
+      },
+      async authorize(credentials) {
+        await connectDB();
+        const userFound = await User.findOne({
+          username: credentials?.username,
+        });
+        if (!userFound) {
+          throw new Error("Invalid credentials");
+        }
+
+        const passwordMatch = await bcrypt.compare(
+          credentials!.password,
+          userFound.password
+        );
+        if (!passwordMatch) {
+          throw new Error("Invalid credentials");
+        }
+        return userFound;
+      },
+    }),
+  ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+        return token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = token.user as any;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
+
+export default authOptions;
